40f7acd3c6f95b1be0f6c4e0987bb5c53982af00
Uday Shetty <udayshetty@Udays-MacBook-Air.local>
2019-10-23 09:51:27 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path=require('path') //core module.no need to install
 const express=require('express');
 const app=express();
+const port=process.env.PORT||3000 //gets value if it can  otherwise run 3000(locally)
 const hbs=require('hbs')
 
 const request=require('request')
@@ -98,6 +99,6 @@ app.get('*',(req,res)=>{
 	})
 })
 
-app.listen(3000,()=>{
-	console.log('server has started !!!')
+app.listen(port,()=>{
+	console.log('server has started on'+port)
 })
